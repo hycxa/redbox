5fb6f9c5256f146e266918bd94f493ee55237a44
lisen <lisen04@baidu.com>
2015-06-26 16:50:10 +0800

    提交基本目录结构

---
+++ app/wscss.sh
@@ -0,0 +1,1 @@
+sass --watch --scss scss/main.scss:stylesheets/main.css
---
+++ build.js
@@ -0,0 +1,16 @@
+var Builder = require('node-webkit-builder');
+var nw = new Builder({
+    files: ["package.json", "node_modules/**/**", "app/**/**", "!app/scss/**/**"], // use the glob format
+    platforms: ['osx64']
+});
+
+//Log stuff you want
+
+nw.on('log',  console.log);
+
+// Build returns a promise
+nw.build().then(function () {
+   console.log('all done!');
+}).catch(function (error) {
+    console.error(error);
+});
